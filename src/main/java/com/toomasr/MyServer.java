9d5f5be4264f541ddb6d009029aa146ec92997b7
Toomas Romer <toomas@zeroturnaround.com>
2015-10-20 13:09:56 +0300

    A simple example for embedded Jetty

---
+++ src/main/java/com/toomasr/MyServer.java
@@ -0,0 +1,27 @@
+package com.toomasr;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class MyServer extends AbstractHandler {
+  public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    response.setContentType("text/html; charset=utf-8");
+    response.setStatus(HttpServletResponse.SC_OK);
+    response.getWriter().println("<h1>Hello World</h1>");
+    baseRequest.setHandled(true);
+  }
+
+  public static void main(String[] args) throws Exception {
+    Server server = new Server(8080);
+    server.setHandler(new MyServer());
+    server.start();
+    server.join();
+  }
+}
